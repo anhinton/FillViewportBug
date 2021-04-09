621a17085a88ee6a9b1c49dcb71698bb4d3542df
Ashley Noel Hinton <ashley@canadia.co.nz>
2021-04-10 09:09:12 +1200

    Simplify example by displaying single texture the size of the Viewport

--- core/src/com/mygdx/fillviewportbug/FillViewportBug.java
+++ core/src/com/mygdx/fillviewportbug/FillViewportBug.java
@@ -6,28 +6,20 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FillViewport;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class FillViewportBug extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	private Texture bannerLeftTexture;
-	private Texture bannerRightTexture;
-	private Viewport fitViewport;
+	private Texture bannerTexture;
 	private Viewport fillViewport;
 
 	@Override
 	public void create () {
 		fillViewport = new FillViewport(1280, 720);
 
-		fitViewport = new FitViewport(720, 1280);
-
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 
-		bannerLeftTexture = new Texture("banner_left.jpg");
-		bannerRightTexture = new Texture("banner_right.jpg");
+		bannerTexture = new Texture("banner.jpg");
 	}
 
 	@Override
@@ -35,35 +27,22 @@ public class FillViewportBug extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-		// Draw "game" area
-		fitViewport.apply();
-		batch.setProjectionMatrix(fitViewport.getCamera().combined);
-		batch.begin();
-		batch.draw(img, fitViewport.getWorldWidth() / 2 - img.getWidth() / 2f,
-				fitViewport.getWorldHeight() / 2 - img.getHeight() / 2f);
-		batch.end();
-
-		// Draw "banner" down sides
 		fillViewport.apply();
 		batch.setProjectionMatrix(fillViewport.getCamera().combined);
 		batch.begin();
-		batch.draw(bannerLeftTexture, 0, 0, fillViewport.getWorldWidth(), fillViewport.getWorldHeight());
-//		batch.draw(bannerRightTexture, fillViewport.getWorldWidth() - bannerRightTexture.getWidth(), 0);
+		batch.draw(bannerTexture, 0, 0, fillViewport.getWorldWidth(), fillViewport.getWorldHeight());
 		batch.end();
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		super.resize(width, height);
-		fitViewport.update(width, height, true);
 		fillViewport.update(width, height, true);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
-		bannerLeftTexture.dispose();
-		bannerRightTexture.dispose();
+		bannerTexture.dispose();
 	}
 }
